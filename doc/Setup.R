b1c1706a5961196ea90128d0b0d12dcd0123af4a
Meera Patel <patelmeeray@gmail.com>
2020-09-13 16:04:24 -0400

    package update

---
+++ doc/Setup.R
@@ -0,0 +1,18 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  echo=TRUE,
+  eval=FALSE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+#  library(skyscraper)
+#  library(chariot)
+
+## -----------------------------------------------------------------------------
+#  conn <- chariot::connectAthena()
+#  skyscraper::createCancerGovSchema(conn = conn)
+#  skyscraper::ddlCancerGovSchema(conn = conn)
+#  chariot::dcAthena(conn=conn)
+
