d0dcec296e442838b625c05d15446b27f6373025
Meera Patel <patelmeeray@gmail.com>
2020-09-16 16:49:42 -0400

    adjust maintain_hemonc_phrase_log job

--- inst/jobs/maintain_hemonc_phrase_log.R
+++ inst/jobs/maintain_hemonc_phrase_log.R
@@ -4,18 +4,24 @@ library(chariot)
 library(pg13)
 library(skyscraper)
 
-
-concepts <- chariot::queryAthena("SELECT DISTINCT concept_name
-                                 FROM public.concept
-                                 WHERE
-                                        vocabulary_id = 'HemOnc'
-                                                AND invalid_reason IS NULL
-                                                AND domain_id = 'Drug';")
-
-concepts <- unlist(concepts)
-
-
 conn <- chariot::connectAthena()
+
+concepts <- chariot::queryAthena("SELECT DISTINCT cs.concept_synonym_name, pl.*
+                                 FROM public.concept c
+                                 LEFT JOIN public.concept_synonym cs
+                                 ON cs.concept_id = c.concept_id
+                                 LEFT JOIN chemidplus.phrase_log pl
+                                 ON pl.input = cs.concept_synonym_name
+                                 WHERE
+                                        c.vocabulary_id = 'HemOnc'
+                                                AND c.invalid_reason IS NULL
+                                                AND c.domain_id = 'Drug';") %>%
+                dplyr::filter_at(vars(!concept_synonym_name),
+                                 all_vars(is.na(.))) %>%
+                dplyr::select(concept_synonym_name) %>%
+                unlist()
+
+
 error_concepts <- vector()
 total_concepts <- length(concepts)
 while (length(concepts)) {
@@ -44,9 +50,22 @@ while (length(concepts)) {
 
         concepts <- concepts[-1]
 
-        secretary::typewrite(secretary::italicize(signif(100*((total_concepts-length(concepts))/total_concepts), digits = 2), "percent completed."))
-        secretary::typewrite(secretary::cyanTxt(length(concepts), "out of", total_concepts, "to go."))
-        secretary::typewrite(secretary::redTxt(length(error_concepts), "errors."))
+        if (interactive()) {
+
+                secretary::typewrite(secretary::italicize(signif(100*((total_concepts-length(concepts))/total_concepts), digits = 2), "percent completed."))
+                secretary::typewrite(secretary::cyanTxt(length(concepts), "out of", total_concepts, "to go."))
+                secretary::typewrite(secretary::redTxt(length(error_concepts), "errors."))
+
+        } else {
+
+                report_filename <- paste0("~/Desktop/maintain_hemonc_phrase_log_", as.character(Sys.Date()), ".txt")
+
+                cat("[", as.character(Sys.time()), "]", sep = "", file = report_filename)
+                cat("\t", length(concepts), "/", total_concepts, " (", signif(100*((total_concepts-length(concepts))/total_concepts), digits = 2), " percent completed)\n", sep = "", file = report_filename, append = TRUE)
+                cat("[", as.character(Sys.time()), "]", sep = "", file = report_filename, append = TRUE)
+                cat("\t", length(error_concepts), " errors\n", sep = "", file = report_filename, append = TRUE)
+
+        }
 }
 
 
@@ -60,7 +79,7 @@ while (length(concepts)) {
                 tryCatch(
                         skyscraper::getRN(conn = conn,
                               input = concept,
-                              sleep_secs = 8),
+                              sleep_secs = 10),
                         error = function(e) paste("Error")
                 )
 
@@ -78,9 +97,70 @@ while (length(concepts)) {
 
         concepts <- concepts[-1]
 
-        secretary::typewrite(secretary::italicize(signif(100*((total_concepts-length(concepts))/total_concepts), digits = 2), "percent completed."))
-        secretary::typewrite(secretary::cyanTxt(length(concepts), "out of", total_concepts, "to go."))
-        secretary::typewrite(secretary::redTxt(length(error_concepts), "errors."))
+        if (interactive()) {
+
+                secretary::typewrite(secretary::italicize(signif(100*((total_concepts-length(concepts))/total_concepts), digits = 2), "percent completed."))
+                secretary::typewrite(secretary::cyanTxt(length(concepts), "out of", total_concepts, "to go."))
+                secretary::typewrite(secretary::redTxt(length(error_concepts), "errors."))
+
+        } else {
+
+                report_filename <- paste0("~/Desktop/maintain_hemonc_phrase_log_", as.character(Sys.Date()), ".txt")
+
+                cat("[", as.character(Sys.time()), "]", sep = "", file = report_filename)
+                cat("\t", length(concepts), "/", total_concepts, " (", signif(100*((total_concepts-length(concepts))/total_concepts), digits = 2), " percent completed)\n", sep = "", file = report_filename, append = TRUE)
+                cat("[", as.character(Sys.time()), "]", sep = "", file = report_filename, append = TRUE)
+                cat("\t", length(error_concepts), " errors\n", sep = "", file = report_filename, append = TRUE)
+
+        }
 }
+
+concepts <- error_concepts
+error_concepts <- vector()
+total_concepts <- length(concepts)
+while (length(concepts)) {
+        concept <- concepts[1]
+
+        output <-
+                tryCatch(
+                        skyscraper::getRN(conn = conn,
+                                          input = concept,
+                                          sleep_secs = 15),
+                        error = function(e) paste("Error")
+                )
+
+
+        if (length(output)) {
+
+                if (output == "Error") {
+
+                        error_concepts <-
+                                c(error_concepts,
+                                  concept)
+
+                }
+        }
+
+        concepts <- concepts[-1]
+
+        if (interactive()) {
+
+                secretary::typewrite(secretary::italicize(signif(100*((total_concepts-length(concepts))/total_concepts), digits = 2), "percent completed."))
+                secretary::typewrite(secretary::cyanTxt(length(concepts), "out of", total_concepts, "to go."))
+                secretary::typewrite(secretary::redTxt(length(error_concepts), "errors."))
+
+        } else {
+
+                report_filename <- paste0("~/Desktop/maintain_hemonc_phrase_log_", as.character(Sys.Date()), ".txt")
+
+                cat("[", as.character(Sys.time()), "]", sep = "", file = report_filename)
+                cat("\t", length(concepts), "/", total_concepts, " (", signif(100*((total_concepts-length(concepts))/total_concepts), digits = 2), " percent completed)\n", sep = "", file = report_filename, append = TRUE)
+                cat("[", as.character(Sys.time()), "]", sep = "", file = report_filename, append = TRUE)
+                cat("\t", length(error_concepts), " errors\n", sep = "", file = report_filename, append = TRUE)
+
+        }
+}
+
+
 
 chariot::dcAthena(conn = conn)
