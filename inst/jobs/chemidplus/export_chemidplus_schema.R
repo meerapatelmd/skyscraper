0ce55782b9522fab0e7e9c5c86dc3b82d3703548
Meera Patel <patelmeeray@gmail.com>
2020-09-18 19:02:06 -0400

    bug fix export job

--- inst/jobs/chemidplus/export_chemidplus_schema.R
+++ inst/jobs/chemidplus/export_chemidplus_schema.R
@@ -7,7 +7,10 @@ library(rubix)
 library(glitter)
 library(sinew)
 
-outputPath <- path.expand("~/GitHub/chemidplusData/data-raw/")
+current_wd <- getwd()
+target_dir <- path.expand("~/GitHub/chemidplusData/")
+setwd(target_dir)
+outputPath <- paste0(target_dir, "data-raw/")
 
 conn <- chariot::connectAthena()
 chemiTables <-
@@ -35,13 +38,18 @@ chemiTables %>%
                     function(x, y) paste0(x, " <- broca::simply_read_csv('", outputPath, y, "')")) %>%
         unlist()
 
-usethisLines <-
-        paste0("usethis::use_data(", paste(chemiTables, collapse = ","), ", overwrite = TRUE)")
+# setWdLines <-
+#         paste0("\nsetwd('",dirname(outputPath), "')")
 
-readr::write_lines(c("library(broca)",
+usethisLines <-
+        paste0("\nusethis::use_data(", paste(chemiTables, collapse = ","), ", overwrite = TRUE)")
+
+readr::write_lines(c("library(broca)\n",
                      loadLines,
+                     #setWdLines,
                      usethisLines),
                    path = paste0(outputPath, "DATASET.R"))
+
 
 source(paste0(outputPath, "DATASET.R"),
        local = TRUE)
@@ -62,7 +70,6 @@ paste0("chemiData$", names(chemiData)) %>%
         cat(file = "~/GitHub/chemidplusData/R/data.R")
 
 
-current_wd <- getwd()
-setwd("~/GitHub/chemidplusData/")
 glitter::docPushInstall(commit_message = "automated data refresh", has_vignettes = FALSE)
+
 setwd(current_wd)
