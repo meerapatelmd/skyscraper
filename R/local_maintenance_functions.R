2fc3d20a9347297c68c920fb41aee35afca7c4b7
Meera Patel <patelmeeray@gmail.com>
2020-10-02 01:09:39 -0400

    move consolidated files to deprecated folder

--- R/local_maintenance_functions.R
+++ R/local_maintenance_functions.R
@@ -19,6 +19,44 @@
 #' @name local_maintenance_functions
 NULL
 
+
+#' @title
+#' Map a skyscraper Schema to a Data Package Repository
+#'
+#' @description
+#' Each Postgres schema governed by this package has a corresponding Data Package, where updates to the Postgres schema are exported as csvs, packaged, and pushed to a GitHub repo.
+#'
+#' @param repo_username         Github username to which the Data Package belongs, Default: 'meerapatelmd'
+#' @seealso
+#'  \code{\link[tibble]{tribble}}
+#'  \code{\link[dplyr]{mutate}}
+#' @rdname map_schema
+#' @family local maintenance
+#' @export
+#' @importFrom tibble tribble
+#' @importFrom dplyr mutate
+#' @importFrom magrittr %>%
+
+
+map_schema <-
+        function(repo_username = "meerapatelmd") {
+
+
+                tibble::tribble(~schema, ~dataPackage, ~tables,
+                                "cancergov", "cancergovData", "c('DRUG_DICTIONARY', 'DRUG_DICTIONARY_LOG', 'DRUG_LINK', 'DRUG_LINK_SYNONYM', 'DRUG_LINK_URL', 'DRUG_LINK_NCIT')",
+                                "chemidplus_search", "chemidplusSearchData", "c('CLASSIFICATION', 'LINKS_TO_RESOURCES', 'NAMES_AND_SYNONYMS', 'REGISTRY_NUMBER_LOG', 'REGISTRY_NUMBERS', 'RN_URL_VALIDITY')",
+                                "chemidplus", "chemidplusData", "c('CLASSIFICATION', 'LINKS_TO_RESOURCES', 'NAMES_AND_SYNONYMS', 'REGISTRY_NUMBER_LOG', 'REGISTRY_NUMBERS', 'RN_URL_VALIDITY')",
+                                "pubmed_search", "pubmedSearchData", NA) %>%
+                        dplyr::mutate(repo = paste0(repo_username, "/", dataPackage))
+
+        }
+
+
+
+
+
+
+
 #' @title
 #' Get the Status of a Data Package Installation
 #'
